f24f20b2162b114006c792cc39b96e692afcce73
fuchaili <lifuchai@139.com>
2018-04-02 12:52:09 +0800

    lk

---
+++ flag.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main()  {
+	fileName:=flag.String("file","fileName","input a filename")
+	flag.Parse()
+	fmt.Printf("%s\n",*fileName)
+	fmt.Printf("%T\n",fileName)
+	fmt.Printf("%T\n",*fileName)
+	fmt.Println("filename",fileName)
+
+}
